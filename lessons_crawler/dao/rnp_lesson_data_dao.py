9792b72b0a6ab326420b875bcb4768ad65cecc26
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-17 00:58:14 -0300

    [#2][enh] Creating RNPLessonDAO to extract data from RNP files

---
+++ lessons_crawler/dao/rnp_lesson_data_dao.py
@@ -0,0 +1,44 @@
+from collections import OrderedDict
+
+import requests
+
+from helpers.xml_parser import XMLParser
+from models.lessons import Lesson
+
+
+class RNPLessonDataDAO:
+    def __init__(self, lesson: Lesson):
+        self.lesson = lesson
+
+        self.__xml_data = requests.get(lesson.xml_file)
+        self.__xml_data_parsed = XMLParser(self.__xml_data.text, self.__xml_data.encoding)
+
+        self.__index_data = requests.get(lesson.index_file)
+        self.__index_data_parsed = XMLParser(self.__index_data.text, self.__index_data.encoding)
+
+        self.__sync_data = requests.get(lesson.sync_file)
+        self.__sync_data_parsed = XMLParser(self.__sync_data.text, self.__sync_data.encoding)
+
+    @property
+    def xml_raw_data(self) -> str:
+        return self.__xml_data_parsed.text
+
+    @property
+    def xml_json_data(self) -> OrderedDict:
+        return self.__xml_data_parsed.parsed
+
+    @property
+    def index_raw_data(self) -> str:
+        return self.__index_data_parsed.text
+
+    @property
+    def index_json_data(self) -> OrderedDict:
+        return self.__index_data_parsed.parsed
+
+    @property
+    def sync_raw_data(self) -> str:
+        return self.__sync_data_parsed.text
+
+    @property
+    def sync_json_data(self) -> OrderedDict:
+        return self.__sync_data_parsed.parsed
