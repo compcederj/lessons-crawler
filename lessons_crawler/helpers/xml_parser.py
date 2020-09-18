35225bb8beb010c77ff9bf29b543faa9ca0c5e0d
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-17 23:45:35 -0300

    [#2][enh] Avoiding missing encoding for XMLParser

--- lessons_crawler/helpers/xml_parser.py
+++ lessons_crawler/helpers/xml_parser.py
@@ -5,6 +5,6 @@ import xmltodict
 
 class XMLParser:
     def __init__(self, text: str, encoding: str = "utf-8"):
-        self.encoding = encoding
+        self.encoding = encoding if encoding else "utf-8"
         self.text = text.encode(self.encoding).decode()
         self.parsed = xmltodict.parse(self.text)
